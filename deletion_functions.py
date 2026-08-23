07ee33232e0c049d272831360c602af376af1774
GunmanRay <rayyy477@gmail.com>
2026-08-23 22:08:22 +0800

    Created a simple working deletion function

--- deletion_functions.py
+++ deletion_functions.py
@@ -8,11 +8,11 @@ from validation_functions import RECOGNIZED_SS_EXTENSIONS
 
 FILE_PATH = os.path.realpath(__file__)
 
-def delete_from_cwd(cwd=None, use_recursion=False):
+def delete_from_cwd(cwd=None, delete_from_subdirs=False):
     cwd = hf.cwd_none_check(cwd)
     hf.path_validation(cwd)
-    to_delete = ob.get_save_states(cwd=cwd, use_recursion=use_recursion)
+    to_delete = ob.get_save_states(cwd=cwd, use_recursion=delete_from_subdirs)
 
     for save_state in to_delete:
         save_state_path = os.path.join(cwd, save_state)
-        os.remove(save_state)
+        os.remove(save_state_path)
