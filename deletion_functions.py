d6ffdd1d1e0ade5f55cae4333b9f44e910d9dc35
GunmanRay <rayyy477@gmail.com>
2026-08-23 22:07:56 +0800

    Created a simple working deletion function

--- deletion_functions.py
+++ deletion_functions.py
@@ -1,6 +1,18 @@
 # Code for handling the deletion of functions and parsing of path links. 
 import os
 import sys
+import obtain_functions as ob
+import helper_functions as hf
 from validation_functions import RECOGNIZED_SS_EXTENSIONS
 
+
 FILE_PATH = os.path.realpath(__file__)
+
+def delete_from_cwd(cwd=None, use_recursion=False):
+    cwd = hf.cwd_none_check(cwd)
+    hf.path_validation(cwd)
+    to_delete = ob.get_save_states(cwd=cwd, use_recursion=use_recursion)
+
+    for save_state in to_delete:
+        save_state_path = os.path.join(cwd, save_state)
+        os.remove(save_state)
