8d652e9f8ac1077de7a66158810c90a8bb6ab000
GunmanRay <rayyy477@gmail.com>
2026-08-24 22:51:35 +0800

    feat:Added ability to delete save states from a text file of folders

--- deletion_functions.py
+++ deletion_functions.py
@@ -17,7 +17,7 @@ def delete_from_cwd(cwd=None, delete_from_subdirs=False):
         save_state_path = os.path.join(cwd, save_state)
         os.remove(save_state_path)
 
-def delete_from_text_file():
+def delete_from_states_txt():
     if not os.path.exists(STATES):
         print("Creating a text file to hold save states...")
         with open(STATES, "r") as state_file:
@@ -25,12 +25,12 @@ def delete_from_text_file():
             return 
     else:
         with open(STATES, "r") as states:
-            save_states = states.readlines()
+            save_states = states.read().splitlines()
             for save_state in save_states:
                 hf.path_validation(save_state)
                 os.remove(save_state)
 
-def delete_from_folders(delete_from_subdirs=False):
+def delete_from_folders_txt(delete_from_subdirs=False):
     if not os.path.exists(FOLDERS):
         print("Creating a text file to hold save states...")
         with open(FOLDERS, "r") as state_file:
@@ -39,6 +39,19 @@ def delete_from_folders(delete_from_subdirs=False):
 
     else:
         with open(FOLDERS, "r") as directories:
-            folders = directories.readlines()
+            folders = directories.read().splitlines()
             for folder in folders:
                 delete_from_cwd(cwd=folder, delete_from_subdirs=delete_from_subdirs)
+
+def delete_from_text_file_of_dirs(text_file, delete_from_subdirs=False):
+    hf.path_validation(text_file)
+    with open(text_file, "r") as folder_file:
+        directories = folder_file.read().splitlines()
+
+    # Make sure every line here is a valid directory
+    for directory in directories:
+        if not os.path.isdir(directory): 
+            raise FileNotFoundError(f"{directory} is not recognized as a folder")
+
+    for directory in directories:
+        delete_from_cwd(cwd=directory, delete_from_subdirs=delete_from_subdirs)
